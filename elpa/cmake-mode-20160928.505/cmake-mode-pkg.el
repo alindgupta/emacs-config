d49169f7121e34f97e20f1a9cf60179f780e7940
Alind Gupta <alindgupta@alinds-mbp.gateway.2wire.net>
2018-02-05 22:33:09 -0500

    elpa

---
+++ elpa/cmake-mode-20160928.505/cmake-mode-pkg.el
@@ -0,0 +1,2 @@
+;;; -*- no-byte-compile: t -*-
+(define-package "cmake-mode" "20160928.505" "major-mode for editing CMake sources" 'nil :commit "b1a06e472607efac5dded07ba0ca28c451f169dc")
---
+++ elpa/flycheck-rtags-20170522.2154/flycheck-rtags-pkg.el
@@ -0,0 +1,2 @@
+;;; -*- no-byte-compile: t -*-
+(define-package "flycheck-rtags" "20170522.2154" "RTags Flycheck integration." '((emacs "24") (flycheck "0.23") (rtags "2.10")) :commit "7fa54d513fc716b2dc1055636b4728ab29dfdd3e" :url "http://rtags.net")
