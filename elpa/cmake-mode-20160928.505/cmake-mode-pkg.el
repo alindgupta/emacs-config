26d9ebe50e9dcb0dc2e25f08a021c935efb9f462
Alind Gupta <alindgupta@alinds-mbp.gateway.2wire.net>
2018-02-05 22:47:24 -0500

    none

--- elpa/cmake-mode-20160928.505/cmake-mode-pkg.el
+++
@@ -1,2 +0,0 @@
-;;; -*- no-byte-compile: t -*-
-(define-package "cmake-mode" "20160928.505" "major-mode for editing CMake sources" 'nil :commit "b1a06e472607efac5dded07ba0ca28c451f169dc")
--- elpa/rtags-20171010.1544/rtags-pkg.el
+++
@@ -1,2 +0,0 @@
-;;; -*- no-byte-compile: t -*-
-(define-package "rtags" "20171010.1544" "A front-end for rtags" 'nil :commit "8543d23b00846bd1c7af9dde30d66ddddfd86297" :url "http://rtags.net")
